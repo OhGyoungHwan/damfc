c8c49913bdcf3108826866bf5c9fba30145248f4
경환오 <rudghks531@naver.com>
2023-12-14 14:37:52 +0900

    localhost to 127.0.0.1

--- src/app/types/queryKeys.ts
+++ src/app/types/queryKeys.ts
@@ -2,14 +2,14 @@ import { IClubRES } from "../api/club/route";
 import { IRecommendResponse } from "../api/recommend/route";
 
 export const getClub = async () =>
-  (await fetch(
-    "https://damfc-3y0v45jwj-ohgyounghwans-projects.vercel.app/api/club"
-  ).then((res) => res.json())) as IClubRES;
+  (await fetch("http://127.0.0.1:3000/api/club").then((res) =>
+    res.json()
+  )) as IClubRES;
 
 export const getRecommend = async () =>
-  (await fetch(
-    "https://damfc-3y0v45jwj-ohgyounghwans-projects.vercel.app/api/recommend"
-  ).then((res) => res.json())) as IRecommendResponse;
+  (await fetch("http://127.0.0.1:3000/api/recommend").then((res) =>
+    res.json()
+  )) as IRecommendResponse;
 
 const GET_CLUB = "get-api-club";
 const GET_RECOMMEND = "get-api-recommend";
