1f3561478f5ece50bb5676b3252f41c3ebd07737
경환오 <rudghks531@naver.com>
2023-12-14 15:16:35 +0900

    bug test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
+import Clubs from "./component/Clubs";
+import Recommends from "./component/Recommends";
 import Hydrate from "./utils/Hydrate";
-// import { useSSRGetHome } from "./hooks/queries";
+import { useSSRGetHome } from "./hooks/queries";
 
 export default async function Home() {
-  // const { dehydratedState } = await useSSRGetHome();
+  const { dehydratedState } = await useSSRGetHome();
   return (
     <main className="flex min-h-screen flex-col items-center px-[16px] compact:pl-[88px] compact:pr-[32px] py-[32px] gap-[96px]">
-      {/* <Hydrate state={dehydratedState}> */}
-      {/* <Recommends />
-        <Clubs /> */}
-      {/* </Hydrate> */}
+      <Hydrate state={dehydratedState}>
+        <Recommends />
+        <Clubs />
+      </Hydrate>
     </main>
   );
 }
--- src/app/types/queryKeys.ts
+++ src/app/types/queryKeys.ts
@@ -2,14 +2,14 @@ import { IClubRES } from "../api/club/route";
 import { IRecommendResponse } from "../api/recommend/route";
 
 export const getClub = async () =>
-  (await fetch("http://127.0.0.1:3000/api/club").then((res) =>
-    res.json()
+  (await fetch("https://damfc-ohgyounghwans-projects.vercel.app/api/club").then(
+    (res) => res.json()
   )) as IClubRES;
 
 export const getRecommend = async () =>
-  (await fetch("http://127.0.0.1:3000/api/recommend").then((res) =>
-    res.json()
-  )) as IRecommendResponse;
+  (await fetch(
+    "https://damfc-ohgyounghwans-projects.vercel.app/api/recommend"
+  ).then((res) => res.json())) as IRecommendResponse;
 
 const GET_CLUB = "get-api-club";
 const GET_RECOMMEND = "get-api-recommend";
